db91bb5d2948a951903d1705adfe66639ca8ee74
junuunes <juliana.nds19@gmail.com>
2025-08-21 17:26:40 -0300

    Atualização projeto

---
+++ src/ContaTerminal.java
@@ -0,0 +1,29 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        int numero;
+        String agencia;
+        String nomeCliente;
+        Double saldo;
+
+        System.out.println("Bem-vindo ao Banco DIO!");
+
+        System.out.println("Por favor, digite o número da sua conta: ");
+        numero = scanner.nextInt();
+
+        System.out.println("Por favor, digite o número da sua agência: ");
+        agencia = scanner.next();
+
+        System.out.println("Por favor, digite seu nome: ");
+        nomeCliente = scanner.next();
+
+        System.out.println("Por favor, digite o saldo: ");
+        saldo = scanner.nextDouble();
+
+    System.out.println("Olá " + nomeCliente +", obrigado por criar uma conta em nosso banco, sua agência é " + agencia +", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque.");
+    }
+
+}
